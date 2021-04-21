a40713128474a816463398b276a3dd1aa8d1007f
Sudhanshu Malla <sudhanshu2.malla@live.uwe.ac.uk>
2021-04-21 15:01:30 +0100

    Assessment Task 4 deployed | Maven project

---
+++ Enterprise_Architecture/Assessement_Task_4/src/main/java/com/camunda/myBPMdeployemnt/deploymentBPM/CheckDeliveryDelecate.java
@@ -0,0 +1,14 @@
+package com.camunda.myBPMdeployemnt.deploymentBPM;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CheckDeliveryDelecate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Enterprise_Architecture/Assessement_Task_4/src/main/java/com/camunda/myBPMdeployemnt/deploymentBPM/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.camunda.myBPMdeployemnt.deploymentBPM;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "deploymentBPM"; // BPMN Process ID
+
+}
